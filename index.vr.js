b5fa3d2f00fcb182bfbee6faaed5d9862f4b21a8
Tessa Gonzales <tessa.l.gonzales@aexp.com>
2019-03-26 15:44:50 -0700

    test push to branch

--- index.vr.js
+++ index.vr.js
@@ -7,6 +7,7 @@ import Main from './components/Main'
 
 export default class benefits_demo extends React.Component {
   render() {
+    console.log('working here')
     return (
       <View>
         <Main />
